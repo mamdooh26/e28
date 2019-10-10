fa99ff0b0639e2bc50f1ed8c7acc35399d8e6bd2
Mamdouh Alharbi <mamdooh26@hotmail.com>
2019-10-11 02:59:05 +0300

    commit p1

---
+++ p1/README.md
@@ -0,0 +1,9 @@
+# Project 1
++ By: Mamdouh Alharbi
++ Production URL: <http://p1.hesweb.xyz>
+
+## Outside resources
+None
+
+## Notes for instructor
+None
---
+++ p1/script.js
@@ -0,0 +1,55 @@
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        userInput:null,
+        failed: false,
+        low: false,
+        gameOver: false,
+        attempt: 3,
+        high: false,
+        feedback: false,
+        correct: false,
+        RandomNumber: null
+    },
+    methods: {
+        GuessNumber: function () {
+            this.correct = this.userInput == this.RandomNumber;
+            this.feedback = true;
+            if (this.attempt <= 1){
+                this.gameOver = true
+                this.high = false
+                this.low = false 
+            }else
+            {
+                if(this.userInput > this.RandomNumber && this.attempt > 0){
+                    this.high = true
+                    this.low = false
+                    this.failed = true 
+                    this.attempt --   
+                }
+                else if(this.userInput < this.RandomNumber && this.attempt > 0) {
+                    this.low = true
+                    this.high = false
+                    this.failed = true
+                    this.attempt --
+                }
+        }
+        },
+
+        PlayAgain: function () {
+            this.correct = false
+            this.high = false
+            this.low = false
+            this.gameOver = false
+            this.failed = false
+            this.attempt = 3
+            this.RandomNumber = this.RandomNumber = Math.floor(Math.random() * Math.floor(100));
+        }
+    },
+
+    created () {
+        this.RandomNumber = this.RandomNumber = Math.floor(Math.random() * Math.floor(100));
+    }
+    
+});
